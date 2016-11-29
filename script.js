2633454ff6e342c939fc0a5f3f1aead232d8f6b8
ZacSmir <Zach@smirin.us>
2016-11-29 08:43:02 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var button = $(,.)
+var button = $(".deal-with-it-button")
 
 $(function() {
   button.click(dealWithIt)
 })
 
 function dealWithIt() {
-    $("#glasses").animate({left: "+=50"}, 1500)
+    $("#glasses").animate({down: "-200"}, 1500)
 }
